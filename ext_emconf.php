a506131398f2a0910d2a5abc62e863f927ed4983
rtpartner <rtpartner@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-01-16 13:54:55 +0000

    [INFO] Importing updated version no.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/smarty_demo/trunk@7930 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "smarty_demo"
 #
-# Auto generated 08-01-2008 06:03
+# Auto generated 16-01-2008 12:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'smarty' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"2d68";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"f477";s:17:"ext_localconf.php";s:4:"234e";s:14:"ext_tables.php";s:4:"16df";s:24:"ext_typoscript_setup.txt";s:4:"f12d";s:16:"locallang_db.xml";s:4:"c993";s:14:"doc/manual.pdf";s:4:"f348";s:31:"pi1/class.tx_smartydemo_pi1.php";s:4:"0e57";s:17:"pi1/locallang.xml";s:4:"24cd";s:23:"res/css/smarty_demo.css";s:4:"ab13";s:20:"res/img/download.gif";s:4:"e921";s:20:"res/img/external.gif";s:4:"a01e";s:19:"res/img/image_1.jpg";s:4:"1d2d";s:19:"res/img/image_2.jpg";s:4:"396a";s:19:"res/img/image_3.jpg";s:4:"c703";s:19:"res/img/image_4.gif";s:4:"a70e";s:20:"res/img/internal.gif";s:4:"9a5e";s:18:"res/img/mailto.gif";s:4:"ab71";s:25:"res/l10n/localization.xml";s:4:"2830";s:28:"res/plugins/block.substr.php";s:4:"0a1a";s:31:"res/plugins/function.repeat.php";s:4:"4d8c";s:27:"res/templates/template.html";s:4:"5e2f";}',
+	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"0439";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"f477";s:17:"ext_localconf.php";s:4:"234e";s:14:"ext_tables.php";s:4:"16df";s:24:"ext_typoscript_setup.txt";s:4:"f12d";s:16:"locallang_db.xml";s:4:"c993";s:14:"doc/manual.pdf";s:4:"f6d7";s:14:"doc/manual.sxw";s:4:"e952";s:31:"pi1/class.tx_smartydemo_pi1.php";s:4:"0e57";s:17:"pi1/locallang.xml";s:4:"24cd";s:23:"res/css/smarty_demo.css";s:4:"ab13";s:20:"res/img/download.gif";s:4:"e921";s:20:"res/img/external.gif";s:4:"a01e";s:19:"res/img/image_1.jpg";s:4:"1d2d";s:19:"res/img/image_2.jpg";s:4:"396a";s:19:"res/img/image_3.jpg";s:4:"c703";s:19:"res/img/image_4.gif";s:4:"a70e";s:20:"res/img/internal.gif";s:4:"9a5e";s:18:"res/img/mailto.gif";s:4:"ab71";s:25:"res/l10n/localization.xml";s:4:"2830";s:28:"res/plugins/block.substr.php";s:4:"0a1a";s:31:"res/plugins/function.repeat.php";s:4:"4d8c";s:27:"res/templates/template.html";s:4:"e854";}',
 	'suggests' => array(
 	),
 );
